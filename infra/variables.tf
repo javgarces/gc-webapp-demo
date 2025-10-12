d99c9053c958ca984723f7c963fb83e8e3b8906b
Javier Garces <35841164+javgarces@users.noreply.github.com>
2025-10-12 12:51:18 +1100

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,30 +1,30 @@
 variable "project_id" {
-  description = "The GCP project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "Region to deploy resources in"
-  type        = string
-  default     = "australia-southeast1"
-}
-
-variable "container_image" {
-  description = "Docker image to deploy to Cloud Run"
-  type        = string
-}
-
-variable "support_email" {
-  description = "Email for OAuth consent screen"
-  type        = string
-}
-
-variable "allowed_user_email" {
-  description = "Email allowed to access IAP"
+  description = "GCP Project ID"
   type        = string
 }
 
 variable "deployer_sa_email" {
   description = "Service account email for GitHub Actions deployments"
   type        = string
+}
+
+variable "wif_provider" {
+  description = "Workload Identity Federation provider full path"
+  type        = string
+}
+
+variable "allowed_user_email" {
+  description = "Email allowed to access IAP-protected backend"
+  type        = string
+}
+
+variable "support_email" {
+  description = "Email used for OAuth consent screen (IAP brand)"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region for Cloud Run"
+  type        = string
+  default     = "australia-southeast1"
 }
