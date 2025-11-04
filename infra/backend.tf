848bc4adb958d4d8bcad21a5656fda035143d993
Javier Garces <35841164+javgarces@users.noreply.github.com>
2025-11-04 17:15:59 +1100

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -17,3 +17,11 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+resource "google_artifact_registry_repository" "backend_repo" {
+  project       = var.project_id
+  location      = var.region
+  repository_id = var.repo_name
+  description   = "Docker repository for backend images"
+  format        = "DOCKER"
+}
